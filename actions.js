f3632b43bce1507a2a1c01e78efcdc17b9bd85c2
YK <yoshihiko.kakutani@gmail.com>
2018-04-25 18:36:03 +0900

    Refactor js codes

--- actions.js
+++ actions.js
@@ -1,266 +1,284 @@
+// settings
+
 var useFileSaver = true;
 var useSweetAlert = true;
-
-function safeString(x) {
-    return ((x == null) ? '' : x.toString());
-}
 
 Blockly.JavaScript.addReservedWords('code');
 Blockly.JavaScript.addReservedWords('highlightBlock');
 
-function generateCodes(workspace) {
-    var jsElement = document.getElementById('jsCode');
-    var xmlElement = document.getElementById('xmlArea');
-    Blockly.JavaScript.INFINITE_LOOP_TRAP =
-        'if (--window.LoopTrap == 0) throw "Maybe infinite loop!";\n';
-    jsElement.value = Blockly.JavaScript.workspaceToCode(workspace);
-    xmlElement.value = Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(workspace));
-    Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
-    return jsElement.value;
+// general functions
+
+function safeString(x) {
+  return ((x == null) ? '' : x.toString());
 }
 
-function runCode(workspace) {
-    // in fact, workspace is not needed.
-    var code = document.getElementById('jsCode').value;
-    try {
-        window.LoopTrap = 1000000;
-        var answer = null;
-        eval(code);
-        if (answer != null) {
-            alertAndCopy(answer, 'Finally, answer is ');
-        }
-    } catch(e) {
-        window.alert(e);
+function sleep(t) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > t) { break; }
+  }
+}
+
+function smartAlert(m, type) {
+  if (useSweetAlert) {
+    if (type) {
+      swal(m, null, type);
+    } else {
+      swal(m);
     }
+  } else {
+    window.alert(m);
+  }
 }
 
 function alertAndCopy(answer, prefix, suffix) {
-    var message = safeString(prefix) + safeString(answer) + safeString(suffix);
-    if (useSweetAlert) {
-        swal({
-            title: message,
-            text: 'Copy it to clipboard?',
-            showCancelButton: true,
-            type: 'success'
-        }, function(isConfirmed) {
-            if (isConfirmed) {
-                intoClipboard(answer);
-            }
-        });
-    } else {
-        var isConfirmed = window.confirm(messsage);
-        if (isConfirmed) {
-            intoClipboard(answer);
-        }
+  var message = safeString(prefix) + safeString(answer) + safeString(suffix);
+  if (useSweetAlert) {
+    swal({
+      title: message,
+      text: 'Copy it to clipboard?',
+      showCancelButton: true,
+      type: 'success'
+    }, function(isConfirmed) {
+      if (isConfirmed) {
+        intoClipboard(answer);
+      }
+    });
+  } else {
+    var isConfirmed = window.confirm(messsage);
+    if (isConfirmed) {
+      intoClipboard(answer);
     }
+  }
 }
 
 function intoClipboard(s) {
-    var sandbox = document.getElementById('invisibleArea');
-    if (sandbox) {
-        try {
-            sandbox.textContent = safeString(s);
-            sandbox.style.display = 'inline';
-            sandbox.select();
-            document.execCommand('copy');
-            sandbox.blur();
-            sandbox.style.display = 'none';
-            return s;
-        } catch(e) {
-            return false;
-        }
-    } else {
-        return false;
+  var sandbox = document.getElementById('invisibleArea');
+  if (sandbox) {
+    try {
+      sandbox.textContent = safeString(s);
+      sandbox.style.display = 'inline';
+      sandbox.select();
+      document.execCommand('copy');
+      sandbox.blur();
+      sandbox.style.display = 'none';
+      return s;
+    } catch(e) {
+      return false;
     }
-}
-
-function initializeWorkspace(workspace, isConfirmed) {
-    if (isConfirmed) {
-        workspace.clear();
-        Blockly.Xml.domToWorkspace(document.getElementById('initBlocks'), workspace);
-    }
-}
-
-function downloadCode(id, type) {
-    var text = document.getElementById(id).value;
-    var blob = new Blob([text], {type: contentType(type)});
-    var url = URL.createObjectURL(blob);
-    if (useFileSaver) {
-        saveAs(blob, filename(type));
-    } else {
-        try {
-            var link = document.getElementById('saveLink');
-            link.href = url;
-            link.download = filename(type);
-            link.target = '_blank';
-            link.click();
-        } catch(e) {
-            window.alert('Cannot download.');
-        }
-    }
-    URL.revokeObjectURL(url);
-}
-
-function saveBlocks(workspace) {
-    generateCodes(workspace);
-    downloadCode('xmlArea', 'xml');
-}
-
-function clearBlocks(workspace) {
-    if (useSweetAlert) {
-        swal({
-            title: 'Workspace will be reset',
-            text: 'Really clear all blocks?',
-            showCancelButton: true,
-            type: 'warning'
-        }, function(isConfirmed) {
-            initializeWorkspace(workspace, isConfirmed);
-        });
-    } else {
-        var isConfirmed = window.confirm('Really clear all blocks?');
-        initializeWorkspace(workspace, isConfirmed);
-    }
-}
-
-function loadBlocks(workspace) {
-    var fileElement = document.getElementById('file');
-    var reader = new FileReader();
-    reader.addEventListener('load', function() {
-        Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(reader.result), workspace);
-    });
-    reader.readAsText(fileElement.files[0], 'UTF-8');
+  } else {
+    return false;
+  }
 }
 
 function hideAllPane() {
-    document.getElementById('blocklyPane').style.display = 'none';
-    document.getElementById('jsPane').style.display = 'none';
-    document.getElementById('xmlPane').style.display = 'none';
+  document.getElementById('blocklyPane').style.display = 'none';
+  document.getElementById('jsPane').style.display = 'none';
+  document.getElementById('xmlPane').style.display = 'none';
 }
 
 function showPane(id) {
-    hideAllPane();
-    document.getElementById(id).style.display = 'table-row';
+  hideAllPane();
+  document.getElementById(id).style.display = 'table-row';
 }
 
-function filename(type) {
-    var prefix = '';
-    var ext = '.txt';
-    switch (type) {
-    case 'text':
-        prefix = ''; ext = '.xml';
-        break;
-    case 'xml':
-        prefix = 'Blockly-'; ext = '.xml';
-        break;
-    case 'js':
-        prefix = 'JS-'; ext = '.js';
-        break;
-    case 'py':
-        prefix = 'Python-'; ext = '.py';
-        break;
-    }
-    var date = new Date();
-    return prefix +
-        date.getFullYear().toString() +
-        ('0' + (date.getMonth() + 1).toString()).slice(-2) +
-        ('0' + date.getDate().toString()).slice(-2) +
-        ('0' + date.getHours().toString()).slice(-2) +
-        ('0' + date.getMinutes().toString()).slice(-2) +
-        ext;
-}
+// showcase including workspaces
 
-function contentType(type) {
-    var ctype = 'text/plain';
-    switch (type) {
-    case 'text':
-        ctype = 'text/plain';
-    case 'xml':
-        ctype = 'text/xml';
-        break;
-    case 'js':
-        ctype = 'application/javascript';
-        break;
-    case 'py':
-        ctype = 'application/python';
-        break;
-    }
-    return ctype;
-}
+class Showcase {
 
-function autoSave(workspace) {
-    var xml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+  constructor(workspace, js, xml, blocks) {
+    this.workspace = workspace;
+    this.jsElement = document.getElementById(js);
+    this.xmlElement = document.getElementById(xml);
+    this.initBlocks = document.getElementById(blocks);
+  }
+
+  generateCodes() {
+    Blockly.JavaScript.INFINITE_LOOP_TRAP =
+      'if (--window.LoopTrap == 0) throw "Maybe infinite loop!";\n';
+    this.jsElement.value = Blockly.JavaScript.workspaceToCode(this.workspace);
+    this.xmlElement.value = Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(this.workspace));
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+    return this.jsElement.value;
+  }
+
+  runCode() {
+    var code = this.jsElement.value;
     try {
-        if (typeof(Storage) !== 'undefined') {
-            localStorage.setItem('blocks', xml);
-        } else {
-            putCookie('blocks', xml, 180);
-        }
-    } finally {}
-}
+      window.LoopTrap = 1000000;
+      var answer = null;
+      answer = eval(code + '; answer;');
+      if (answer != null) {
+        alertAndCopy(answer, 'Finally, answer is ');
+      }
+    } catch(e) {
+      window.alert(e);
+    }
+  }
 
-function autoLoad(workspace) {
+  initializeWorkspace(isConfirmed) {
+    if (isConfirmed) {
+      this.workspace.clear();
+      Blockly.Xml.domToWorkspace(this.initBlocks, this.workspace);
+    }
+  }
+
+  saveBlocks() {
+    this.generateCodes();
+    downloadCode(xmlElement.id, 'xml');
+  }
+
+  clearBlocks() {
+    var showcase = this;
+    if (useSweetAlert) {
+      swal({
+        title: 'Workspace will be reset',
+        text: 'Really clear all blocks?',
+        showCancelButton: true,
+        type: 'warning'
+      }, function(isConfirmed) {
+        showcase.initializeWorkspace(isConfirmed);
+      });
+    } else {
+      var isConfirmed = window.confirm('Really clear all blocks?');
+      this.initializeWorkspace(isConfirmed);
+    }
+  }
+
+  loadBlocks(id) {
+    var workspace = this.workspace;
+    var fileElement = document.getElementById(id);
+    var reader = new FileReader();
+    reader.addEventListener('load', function() {
+      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(reader.result), workspace);
+    });
+    reader.readAsText(fileElement.files[0], 'UTF-8');
+  }
+
+  autoSave() {
+    var xml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(this.workspace));
+    try {
+      if (typeof(Storage) !== 'undefined') {
+        localStorage.setItem('blocks', xml);
+      } else {
+        this.putCookie('blocks', xml, 180);
+      }
+    } finally {}
+  }
+
+  autoLoad() {
     var xml = null;
     try {
-        if (typeof(Storage) !== "undefined") {
-            xml = localStorage.getItem('blocks');
-        } else {
-            xml = getCookie('blocks');
-        }
+      if (typeof(Storage) !== "undefined") {
+        xml = localStorage.getItem('blocks');
+      } else {
+        xml = getCookie('blocks');
+      }
     } finally {
-        if (xml) {
-            Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xml), workspace);
-        } else {
-            initializeWorkspace(workspace, true);
-        }
+      if (xml) {
+        Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xml), this.workspace);
+      } else {
+        this.initializeWorkspace(true);
+      }
     }
-}
+  }
 
-function putCookie(key, value, days) {
+  putCookie(key, value, days) {
     if (! days) { days = 1; }
     var expires = (days * 24 * 60 * 60).toString();
     var path = '/';
     document.cookie =
-        encodeURIComponent(key) + "=" + encodeURIComponent(value) + "; " +
-        'max-age=' + expires + '; ' +
-        'path=' + path + ';';
-}
+      encodeURIComponent(key) + "=" + encodeURIComponent(value) + "; " +
+      'max-age=' + expires + '; ' +
+      'path=' + path + ';';
+  }
 
-function getCookie(key) {
+  getCookie(key) {
     var name = encodeURIComponent(key) + "=";
     var value = '';
     var allPieces = document.cookie.split(';');
     for (var i = 0; i < allPieces.length; i++) {
-        var piece = allPieces[i];
-        while (piece.charAt(0) == ' ') {
-            piece = piece.substring(1);
-        }
-        if (piece.indexOf(name) == 0) {
-            value = piece.substring(name.length, piece.length);
-        }
+      var piece = allPieces[i];
+      while (piece.charAt(0) == ' ') {
+        piece = piece.substring(1);
+      }
+      if (piece.indexOf(name) == 0) {
+        value = piece.substring(name.length, piece.length);
+      }
     }
     while (value.charAt(0) == ' ') {
-        value = value.substring(1);
+      value = value.substring(1);
     }
     return decodeURIComponent(value);
+  }
+
+} // class ends
+
+// downloader
+
+function downloadCode(id, type) {
+  var text = document.getElementById(id).value;
+  var blob = new Blob([text], {type: contentType(type)});
+  var url = URL.createObjectURL(blob);
+  if (useFileSaver) {
+    saveAs(blob, guessFilename(type));
+  } else {
+    try {
+      var link = document.getElementById('saveLink');
+      link.href = url;
+      link.download = guessFilename(type);
+      link.target = '_blank';
+      link.click();
+    } catch(e) {
+      window.alert('Cannot download.');
+    }
+  }
+  URL.revokeObjectURL(url);
 }
 
-function smartAlert(m, type) {
-    if (useSweetAlert) {
-        if (type) {
-            swal(m, null, type);
-        } else {
-            swal(m);
-        }
-    } else {
-        window.alert(m);
-    }
+function guessFilename(type) {
+  var prefix = '';
+  var ext = '.txt';
+  switch (type) {
+  case 'text':
+    prefix = ''; ext = '.xml';
+    break;
+  case 'xml':
+    prefix = 'Blockly-'; ext = '.xml';
+    break;
+  case 'js':
+    prefix = 'JS-'; ext = '.js';
+    break;
+  case 'py':
+    prefix = 'Python-'; ext = '.py';
+    break;
+  }
+  var date = new Date();
+  return prefix +
+    date.getFullYear().toString() +
+    ('0' + (date.getMonth() + 1).toString()).slice(-2) +
+    ('0' + date.getDate().toString()).slice(-2) +
+    ('0' + date.getHours().toString()).slice(-2) +
+    ('0' + date.getMinutes().toString()).slice(-2) +
+    ext;
 }
 
-function sleep(t) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > t) { break; }
-    }
+function contentType(type) {
+  var ctype = 'text/plain';
+  switch (type) {
+  case 'text':
+    ctype = 'text/plain';
+  case 'xml':
+    ctype = 'text/xml';
+    break;
+  case 'js':
+    ctype = 'application/javascript';
+    break;
+  case 'py':
+    ctype = 'application/python';
+    break;
+  }
+  return ctype;
 }
 
 // interpreter
@@ -268,59 +286,68 @@ function sleep(t) {
 var highlightBlock = function (id) {};
 var highlightPause = false;
 
-function initApi(interpreter, scope) {
+class MyInterpreter {
+
+  static initApi(interpreter, scope) {
     // Add an API function for the alert() block.
     var wrapper = function(text) {
-        text = safeString(text);
-        return interpreter.createPrimitive(alert(text));
+      text = safeString(text);
+      return interpreter.createPrimitive(alert(text));
     };
     interpreter.setProperty(scope, 'alert',
                             interpreter.createNativeFunction(wrapper));
     // Add an API function for the prompt() block.
     var wrapper = function(text) {
-        text = safeString(text);
-        return interpreter.createPrimitive(prompt(text));
+      text = safeString(text);
+      return interpreter.createPrimitive(prompt(text));
     };
     interpreter.setProperty(scope, 'prompt',
                             interpreter.createNativeFunction(wrapper));
     // Add an API function for highlighting blocks.
     var wrapper = function(id) {
-        id = safeString(id);
-        return interpreter.createPrimitive(highlightBlock(id));
+      id = safeString(id);
+      return interpreter.createPrimitive(highlightBlock(id));
     };
     interpreter.setProperty(scope, 'highlightBlock',
                             interpreter.createNativeFunction(wrapper));
-}
+  }
 
-function parseCode(workspace) {
+  constructor(workspace) {
+    this.workspace = workspace;
+    this.interpreter = null;
+  }
+
+  parseCode() {
     highlightBlock = function (id) {
-        highlightPause = true;
-        workspace.highlightBlock(id);
+      highlightPause = true;
+      workspace.highlightBlock(id);
     }
     Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
     var code = Blockly.JavaScript.workspaceToCode(workspace);
     Blockly.JavaScript.STATEMENT_PREFIX = null;
-    var myInterpreter = new Interpreter(code, initApi);
+    this.interpreter = new Interpreter(code, MyInterpreter.initApi);
     window.alert('Ready to execute the code.');
     highlightPause = false;
     workspace.highlightBlock(null);
-    return myInterpreter;
-}
+  }
 
-function stepCode(myInterpreter, workspace) {
+  // You need to execute parseCode() in advance.
+  stepCode() {
     var ok = false;
     try {
-        ok = myInterpreter.step();
+      ok = this.interpreter.step();
     } finally {
-        if (!ok) {
-            window.alert('No more executable block.');
-            workspace.highlightBlock(null);
-            return;
-        }
+      if (!ok) {
+        window.alert('No more executable block.');
+        this.workspace.highlightBlock(null);
+        return;
+      }
     }
     if (highlightPause) {
-        highlightPause = false;
+      highlightPause = false;
     } else {
-        stepCode(myInterpreter, workspace);
+      this.stepCode();
     }
-}
+  }
+
+} // class ends
--- blocks.js
+++ blocks.js
@@ -1,49 +1,181 @@
+var myTextsColour = Blockly.Msg.TEXTS_HUE || Blockly.Constants.Text.HUE || 160;
+var myNumColour = Blockly.Msg.MATH_HUE || Blockly.Constants.Math.HUE || 230;
+var myListColour = Blockly.Msg.LISTS_HUE || Blockly.Constants.Lists.HUE || 260;
 
 Blockly.defineBlocksWithJsonArray([
   {
     "type": 'text_2_number',
     "message0": 'get number from text %1',
     "args0": [
-      {
-        "type": 'input_value',
-        "name": 'VALUE',
-        "check": 'String'
-      }
+      {"type": 'input_value', "name": 'VALUE', "check": 'String'},
     ],
     "output": 'Number',
-    "colour": Blockly.Blocks.texts.HUE,
-    "tooltip": 'Return the number meant by the text.'
+    "colour": myTextsColour,
+    "tooltip": 'Return the number denoted by the text.'
   },
   {
     "type": 'number_2_text',
     "message0": 'create text from number %1',
     "args0": [
-      {
-        "type": 'input_value',
-        "name": 'VALUE',
-        "check": 'Number'
-      }
+      {"type": 'input_value', "name": 'VALUE', "check": 'Number'},
     ],
     "output": 'String',
-    "colour": Blockly.Blocks.texts.HUE,
+    "colour": myTextsColour,
     "tooltip": 'Create a text whose content is the number.'
   }
 ]);
 
 Blockly.JavaScript['text_2_number'] =
   function (block) {
-    var argument0 =
-      Blockly.JavaScript.valueToCode(
-        block, 'VALUE', Blockly.JavaScript.ORDER_NONE
-      ) || '\'\'';
-    return ['Number(' + argument0 + ')', Blockly.JavaScript.ORDER_FUNCTION_CALL];
+    var arg =
+        Blockly.JavaScript.valueToCode(
+          block, 'VALUE', Blockly.JavaScript.ORDER_NONE
+        ) || "''";
+    return ['Number(' + arg + ')', Blockly.JavaScript.ORDER_FUNCTION_CALL];
   };
 
 Blockly.JavaScript['number_2_text'] =
   function (block) {
-    var argument0 =
-      Blockly.JavaScript.valueToCode(
-        block, 'VALUE', Blockly.JavaScript.ORDER_NONE
-      ) || '\'\'';
-    return ['(' + argument0 + ').toString()', Blockly.JavaScript.ORDER_MEMBER];
+    var arg =
+        Blockly.JavaScript.valueToCode(
+          block, 'VALUE', Blockly.JavaScript.ORDER_NONE
+        ) || '0';
+    return ['(' + arg + ').toString()', Blockly.JavaScript.ORDER_MEMBER];
+  };
+
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": 'table_row',
+    "message0": 'rows of %1',
+    "args0": [
+      {"type": 'input_value', "name": 'TABLE', "check": "Array"},
+    ],
+    "output": 'Number',
+    "colour": myListColour,
+    "tooltip": 'Return the number of rows in the table.'
+  },
+  {
+    "type": 'table_col',
+    "message0": 'columns of %1',
+    "args0": [
+      {"type": 'input_value', "name": 'TABLE', "check": "Array"},
+    ],
+    "output": 'Number',
+    "colour": myListColour,
+    "tooltip": 'Return the number of culumns in the table.'
+  },
+  {
+    "type": 'table_cell',
+    "message0": 'cell at row %2 col %3 in %1',
+    "args0": [
+      {"type": 'input_value', "name": 'TABLE', "check": "Array"},
+      {"type": 'input_value', "name": 'ROW', "check": 'Number'},
+      {"type": 'input_value', "name": 'COL', "check": 'Number'},
+    ],
+    "output": null,
+    "inputsInline": true,
+    "colour": myListColour,
+    "tooltip": 'Return the content of the specified cell.'
+  },
+  {
+    "type": 'table_setValue',
+    "message0": 'change cell to %4 at row %2 col %3 in %1',
+    "args0": [
+      {"type": 'input_value', "name": 'TABLE', "check": 'Array'},
+      {"type": 'input_value', "name": 'ROW', "check": 'Number'},
+      {"type": 'input_value', "name": 'COL', "check": 'Number'},
+      {"type": 'input_value', "name": 'VALUE'},
+    ],
+    "nextStatement": null,
+    "previousStatement": null,
+    "inputsInline": true,
+    "colour": myListColour,
+    "tooltip": 'Change the content of the specified cell.'
+  },
+  {
+    "type": 'table_create',
+    "message0": 'create table as rows %1 columns %2',
+    "args0": [
+      {"type": 'input_value', "name": 'ROW', "check": 'Number'},
+      {"type": 'input_value', "name": 'COL', "check": 'Number'},
+    ],
+    "output": 'Array',
+    "inputsInline": true,
+    "colour": myListColour,
+    "tooltip": 'Create an empty table that has the specified number of cells.'
+  },
+]);
+
+Blockly.JavaScript['table_row'] =
+  function (block) {
+    var table =
+        Blockly.JavaScript.valueToCode(
+          block, 'TABLE', Blockly.JavaScript.ORDER_MEMBER
+        );
+    if (! table) { return ['0', Blockly.JavaScript.ORDER_ATOMIC]; }
+    return [table + '.length', Blockly.JavaScript.ORDER_MEMBER];
+  };
+
+Blockly.JavaScript['table_col'] =
+  function (block) {
+    var table =
+        Blockly.JavaScript.valueToCode(
+          block, 'TABLE', Blockly.JavaScript.ORDER_MEMBER
+        );
+    if (! table) { return ['0', Blockly.JavaScript.ORDER_ATOMIC]; }
+    return [table + '[0].length', Blockly.JavaScript.ORDER_MEMBER];
+  };
+
+Blockly.JavaScript['table_cell'] =
+  function (block) {
+    var table =
+        Blockly.JavaScript.valueToCode(
+          block, 'TABLE', Blockly.JavaScript.ORDER_MEMBER
+        );
+    var row =
+        Blockly.JavaScript.valueToCode(
+          block, 'ROW', Blockly.JavaScript.ORDER_ADDITION
+        ) || '1';
+    var col =
+        Blockly.JavaScript.valueToCode(
+          block, 'COL', Blockly.JavaScript.ORDER_ADDITION
+        ) || '1';
+    if (! table) { return ['0', Blockly.JavaScript.ORDER_ATOMIC]; }
+    return [table + '[' + row + ' - 1][' + col + ' - 1]', Blockly.JavaScript.ORDER_MEMBER];
+  };
+
+Blockly.JavaScript['table_setValue'] =
+  function (block) {
+    var table =
+        Blockly.JavaScript.valueToCode(
+          block, 'TABLE', Blockly.JavaScript.ORDER_MEMBER
+        );
+    var row =
+        Blockly.JavaScript.valueToCode(
+          block, 'ROW', Blockly.JavaScript.ORDER_ADDITION
+        ) || '1';
+    var col =
+        Blockly.JavaScript.valueToCode(
+          block, 'COL', Blockly.JavaScript.ORDER_ADDITION
+        ) || '1';
+    var value =
+        Blockly.JavaScript.valueToCode(
+          block, 'VALUE', Blockly.JavaScript.ORDER_ASSIGNMENT - 1
+        );
+    if (! table) { return ''; }
+    var cell = table + '[' + row + ' - 1][' + col + ' - 1]';
+    return cell + ' = ' + value + ';' + "\n";
+  };
+
+Blockly.JavaScript['table_create'] =
+  function (block) {
+    var row =
+        Blockly.JavaScript.valueToCode(
+          block, 'ROW', Blockly.JavaScript.ORDER_NONE
+        ) || '2';
+    var col =
+        Blockly.JavaScript.valueToCode(
+          block, 'COL', Blockly.JavaScript.ORDER_NONE
+        ) || '2';
+    return ['Array(' + row + ').fill(0).map(()=> Array(' + col + ').fill(0))', Blockly.JavaScript.ORDER_MEMBER];
   };
